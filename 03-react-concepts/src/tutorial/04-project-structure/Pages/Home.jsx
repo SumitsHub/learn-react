fb9acac6e0712f5a3c1240c6bc79ccce00731304
Sumit Paul <pauls1317@gmail.com>
2023-09-28 00:47:57 +0530

    react-concepts: import/export -> dealing with multiple files

---
+++ 03-react-concepts/src/tutorial/04-project-structure/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../Navbar";
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+export default Home;
---
+++ 03-react-concepts/src/tutorial/04-project-structure/Pages/index.jsx
@@ -0,0 +1,5 @@
+// import Home from './Home';
+import About from "./About";
+
+export { default } from "./Home";
+export { About };
