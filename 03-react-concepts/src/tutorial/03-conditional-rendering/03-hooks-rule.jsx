b6b04298794fc2fd1a101d8fee918f2dd776fbfe
Sumit Paul <pauls1317@gmail.com>
2023-09-28 00:39:03 +0530

    react-concepts: all about conditional rendering

---
+++ 03-react-concepts/src/tutorial/03-conditional-rendering/03-hooks-rule.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const Example = () => {
+  const [condition, setCondition] = useState(true);
+  // if (condition) {
+  //   // won't work -> hooks can only be used inside Functional components not inside any other block
+  //   const [state, setState] = useState(false);
+  // }
+
+  // below useEffect tries to set condition to false after 1 sec
+  useEffect(() => {
+    setTimeout(() => {
+      setCondition(false);
+    }, 1000);
+  }, []);
+
+  if (condition) {
+    return <h2>Hello There</h2>;
+  }
+  // this will also fail -> beacause we are returning before this line reaches AND even if you somehow set condition to false then also it will throw an error
+  // Error: Rendered more hooks than during the previous render -> remember useEffect is a hook
+
+  // useEffect(() => {
+  //   console.log("hello there");
+  // }, []);
+  return <h2>example</h2>;
+};
+
+export default Example;
