104d9abaf3e176005ba2bd46f2e4cad8a89e8320
Sumit Paul <pauls1317@gmail.com>
2023-09-28 19:21:09 +0530

    axios-tutorials: making post request

--- 04-axios-tutorials/README.md
+++ 04-axios-tutorials/README.md
@@ -50,3 +50,17 @@ const fetchDadJoke = async () => {
   }
 };
 ```
+
+#### Post Request
+
+- send data to the server
+- axios.post(url, { data })
+- more options (auth header) - axios.post(url, { data },{})
+
+```js
+try {
+  const resp = await axios.post(url, { data });
+} catch (error) {
+  console.log(error.response.data);
+}
+```
--- 04-axios-tutorials/src/App.jsx
+++ 04-axios-tutorials/src/App.jsx
@@ -1,13 +1,15 @@
 import Title from "./components/Title";
 import FirstRequest from "./examples/1-first-request";
 import Headers from "./examples/2-headers";
+import PostRequest from "./examples/3-post-request";
 
 function App() {
   return (
     <main>
       <Title />
       {/* <FirstRequest /> */}
-      <Headers />
+      {/* <Headers /> */}
+      <PostRequest />
     </main>
   );
 }
