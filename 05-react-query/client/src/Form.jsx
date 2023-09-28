099102a88609442a1ae5bc230f6abc871860413c
Sumit Paul <pauls1317@gmail.com>
2023-09-29 03:33:12 +0530

    react-query/client: invalidate query using query client

--- 05-react-query/client/src/Form.jsx
+++ 05-react-query/client/src/Form.jsx
@@ -1,10 +1,11 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import customFetch from "./utils";
 import { toast } from "react-toastify";
 
 const Form = () => {
   const [newItemName, setNewItemName] = useState("");
+  const queryClient = useQueryClient();
   const {
     mutate: createTask,
     isError,
@@ -12,7 +13,9 @@ const Form = () => {
   } = useMutation({
     mutationFn: taskTitle => customFetch.post("/", { title: taskTitle }),
     onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
       toast.success("Created");
+      setNewItemName("");
     },
     onError: error => {
       console.log(error);
