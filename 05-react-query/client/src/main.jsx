b52256f54cc749f0e14806e88a3d78df0e195052
Sumit Paul <pauls1317@gmail.com>
2023-09-29 03:29:17 +0530

    react-query/client: react-toastify css

--- 05-react-query/client/src/main.jsx
+++ 05-react-query/client/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 
 const queryClient = new QueryClient();
