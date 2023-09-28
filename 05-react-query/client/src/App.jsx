84d958cde2341bad8c134fbec8601361674d8be5
Sumit Paul <pauls1317@gmail.com>
2023-09-28 20:19:13 +0530

    react-query/client: initial code

---
+++ 05-react-query/client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <section className="section-center">
+      <h1>React Query</h1>
+    </section>
+  );
+}
+
+export default App;
