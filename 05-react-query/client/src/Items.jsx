082b20fdc986ca016b287341022927ad2f9a3c09
Sumit Paul <pauls1317@gmail.com>
2023-09-29 03:03:02 +0530

    react-query/client: using queried data in component

--- 05-react-query/client/src/Items.jsx
+++ 05-react-query/client/src/Items.jsx
@@ -2,15 +2,19 @@ import { useQuery } from "@tanstack/react-query";
 import customFetch from "./utils";
 import SingleItem from "./SingleItem";
 
-const Items = ({ items }) => {
+const Items = () => {
   const result = useQuery({
     queryKey: ["tasks"],
     queryFn: () => customFetch.get("/"),
   });
-  console.log(result.data);
+  console.log(result);
+  const { isLoading, data } = result;
+  if (isLoading) {
+    return <p style={{ marginTop: "1rem " }}>Loading...</p>;
+  }
   return (
     <div className="items">
-      {items.map(item => {
+      {data.data.taskList.map(item => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
