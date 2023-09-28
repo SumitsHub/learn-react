aedc057ee3c039120b49cb81a9f5f9696ccc973c
Sumit Paul <pauls1317@gmail.com>
2023-09-28 19:14:31 +0530

    axios-tutorials: first request using axios

--- 04-axios-tutorials/src/App.jsx
+++ 04-axios-tutorials/src/App.jsx
@@ -1,9 +1,11 @@
 import Title from "./components/Title";
+import FirstRequest from "./examples/1-first-request";
 
 function App() {
   return (
     <main>
       <Title />
+      <FirstRequest />
     </main>
   );
 }
