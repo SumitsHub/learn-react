bfabd70228e21ed1e0d10267b8ab7d2cbb2f07e2
Sumit Paul <pauls1317@gmail.com>
2023-09-28 15:43:13 +0530

    react-concepts/performance: slow function without useMemo

--- 03-react-concepts/src/tutorial/11-performance/03-performance-hooks/index.jsx
+++ 03-react-concepts/src/tutorial/11-performance/03-performance-hooks/index.jsx
@@ -1,9 +1,14 @@
 import { useCallback, useState } from "react";
 import { data } from "../../../../data";
 import List from "./List";
+import slowFunction from "./slowFunctions";
+
 const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
+
+  const value = slowFunction(); // this will take few seconds to do the computation and will block everything else till then
+  console.log(value);
 
   const removePerson = useCallback(
     id => {
