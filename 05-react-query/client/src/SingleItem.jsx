08399ac80194efae8a2ba2456e491b59537c9f6c
Sumit Paul <pauls1317@gmail.com>
2023-09-29 03:43:31 +0530

    react-query/client: edit task mutation

--- 05-react-query/client/src/SingleItem.jsx
+++ 05-react-query/client/src/SingleItem.jsx
@@ -1,23 +1,37 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import customFetch from "./utils";
+
 const SingleItem = ({ item }) => {
+  const queryClient = useQueryClient();
+  const { mutate: editTask } = useMutation({
+    mutationFn: ({ taskId, isDone }) => {
+      return customFetch.patch(`/${taskId}`, { isDone });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["tasks"],
+      });
+    },
+  });
   return (
-    <div className='single-item'>
+    <div className="single-item">
       <input
-        type='checkbox'
+        type="checkbox"
         checked={item.isDone}
-        onChange={() => console.log('edit task')}
+        onChange={() => editTask({ taskId: item.id, isDone: !item.isDone })}
       />
       <p
         style={{
-          textTransform: 'capitalize',
-          textDecoration: item.isDone && 'line-through',
+          textTransform: "capitalize",
+          textDecoration: item.isDone && "line-through",
         }}
       >
         {item.title}
       </p>
       <button
-        className='btn remove-btn'
-        type='button'
-        onClick={() => console.log('delete task')}
+        className="btn remove-btn"
+        type="button"
+        onClick={() => console.log("delete task")}
       >
         delete
       </button>
