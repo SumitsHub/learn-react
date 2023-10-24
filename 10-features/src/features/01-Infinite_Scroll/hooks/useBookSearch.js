7a4ae5451ebc03f7418a988d3294fae5f80a5b67
Sumit Paul <pauls1317@gmail.com>
2023-10-24 09:13:26 +0530

    Features: axios cancelToken

--- 10-features/src/features/01-Infinite_Scroll/hooks/useBookSearch.js
+++ 10-features/src/features/01-Infinite_Scroll/hooks/useBookSearch.js
@@ -2,13 +2,21 @@ import { useEffect } from 'react';
 import axios from 'axios';
 export default function useBookSearch(query, pageNumber) {
   useEffect(() => {
+    let cancel = () => {};
     axios({
       method: 'GET',
       url: 'https://openlibrary.org/search.json',
       params: { q: query, page: pageNumber },
-    }).then(res => {
-      console.log(res.data);
-    });
+      cancelToken: new axios.CancelToken(c => (cancel = c)),
+    })
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(() => {
+        return;
+      });
+
+    return () => cancel();
   }, [query, pageNumber]);
   return null;
 }
