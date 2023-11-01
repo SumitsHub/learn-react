2553344b5346cf4cfc6ea6009f0121797019732a
Sumit Paul <pauls1317@gmail.com>
2023-11-01 23:01:11 +0530

    react-apps/tailwind-portfolio: Hero components

--- 11-react-apps/01-tailwind-portfolio/src/App.jsx
+++ 11-react-apps/01-tailwind-portfolio/src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
