c7c59dc3a60332d9973f16b808c930b72bfb2176
Sumit Paul <pauls1317@gmail.com>
2023-11-02 00:02:06 +0530

    react-apps/tailwind-portfolio: custom class in tailwind

--- 11-react-apps/01-tailwind-portfolio/src/components/Hero.jsx
+++ 11-react-apps/01-tailwind-portfolio/src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
 const Hero = () => {
   return (
     <div className="bg-blue-100 py-24">
-      <div className="mx-auto max-w-7xl px-8 grid md:grid-cols-2 items-center gap-8">
+      <div className="align-element px-8 grid md:grid-cols-2 items-center gap-8">
         <article className="text-7xl font-bold tracking-wider">
           <h1 className="text-7xl font-bold tracking-wider">I'm Sumit</h1>
           <p className="mt-4 text-3xl text-slate-700 capitalize tracking-wide">
--- 11-react-apps/01-tailwind-portfolio/src/components/Navbar.jsx
+++ 11-react-apps/01-tailwind-portfolio/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { links } from '../data';
 const Navbar = () => {
   return (
     <nav className="bg-blue-100">
-      <div className="mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-6">
+      <div className="align-element px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-6">
         <h2 className="text-3xl font-bold">
           Web <span className="text-blue-600">Dev</span>
         </h2>
