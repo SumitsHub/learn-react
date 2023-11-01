df6ff1b89c3c8ab51ee339c6664609da9e2799dc
Sumit Paul <pauls1317@gmail.com>
2023-11-02 00:42:17 +0530

    react-apps/tailwind-portfolio: matching blue color theme

--- 11-react-apps/01-tailwind-portfolio/src/data.jsx
+++ 11-react-apps/01-tailwind-portfolio/src/data.jsx
@@ -12,19 +12,19 @@ export const skills = [
   {
     id: nanoid(),
     title: 'HTML&CSS',
-    icon: <FaHtml5 className='h-16 w-16 text-emerald-500' />,
+    icon: <FaHtml5 className="h-16 w-16 text-blue-500" />,
     text: 'Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.',
   },
   {
     id: nanoid(),
     title: 'Javascript',
-    icon: <FaJs className='h-16 w-16 text-emerald-500' />,
+    icon: <FaJs className="h-16 w-16 text-blue-500" />,
     text: 'Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality',
   },
   {
     id: nanoid(),
     title: 'React',
-    icon: <FaReact className='h-16 w-16 text-emerald-500' />,
+    icon: <FaReact className="h-16 w-16 text-blue-500" />,
     text: 'Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.',
   },
 ];
