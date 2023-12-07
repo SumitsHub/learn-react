1c68488cdc99024192e337d4eaba155fa7c2b950
Sumit Paul <pauls1317@gmail.com>
2023-12-07 23:27:19 +0530

    Comfy-Store: Register page

--- 11-react-apps/02-comfy-store/src/pages/Register.jsx
+++ 11-react-apps/02-comfy-store/src/pages/Register.jsx
@@ -1,4 +1,31 @@
+import { FormInput, SubmitBtn } from '../components';
+import { Form, Link } from 'react-router-dom';
 function Register() {
-  return <div className="text-4xl">Register</div>;
+  return (
+    <section className="h-screen grid place-items-center">
+      <Form
+        method="post"
+        className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
+      >
+        <h4 className="text-center text-3xl font-bold">Register</h4>
+
+        <FormInput type="text" label="username" name="username" />
+        <FormInput type="email" label="email" name="email" />
+        <FormInput type="password" label="password" name="password" />
+        <div className="mt-4">
+          <SubmitBtn text="register" />
+        </div>
+        <p className="text-center">
+          Already a member?
+          <Link
+            to="/login"
+            className="mt-2 link link-hover link-primary capitalize"
+          >
+            login
+          </Link>
+        </p>
+      </Form>
+    </section>
+  );
 }
 export default Register;
