ab7027f89702dde91b67374597c54b6ec6a24dbb
Sumit Paul <pauls1317@gmail.com>
2023-12-08 17:20:15 +0530

    Comfy-Store: Navlinks

--- 11-react-apps/02-comfy-store/src/components/Navbar.jsx
+++ 11-react-apps/02-comfy-store/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
+import NavLinks from './NavLinks';
 
 function Navbar() {
   return (
@@ -21,14 +22,16 @@ function Navbar() {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200"
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
             >
-              nav links
+              <NavLinks />
             </ul>
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal">nav links</ul>
+          <ul className="menu menu-horizontal">
+            <NavLinks />
+          </ul>
         </div>
         <div className="navbar-end">
           {/* THEME SETUP */}
