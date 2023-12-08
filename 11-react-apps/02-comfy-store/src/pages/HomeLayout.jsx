898348ca8da09c3c6fd8b5bd957cd9a9f6cf076d
Sumit Paul <pauls1317@gmail.com>
2023-12-08 17:13:54 +0530

    Navbar structure

--- 11-react-apps/02-comfy-store/src/pages/HomeLayout.jsx
+++ 11-react-apps/02-comfy-store/src/pages/HomeLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import { Header } from '../components';
+import { Header, Navbar } from '../components';
 
 function Homelayout() {
   return (
     <>
       <Header />
+      <Navbar />
       <section className="align-elements py-20">
         <Outlet />
       </section>
