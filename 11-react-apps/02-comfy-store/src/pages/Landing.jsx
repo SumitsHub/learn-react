727193639b3cac830d085c03178ad8ec825e7263
Sumit Paul <pauls1317@gmail.com>
2023-12-08 17:57:01 +0530

    Comfy-Store: Landing Hero Component

--- 11-react-apps/02-comfy-store/src/pages/Landing.jsx
+++ 11-react-apps/02-comfy-store/src/pages/Landing.jsx
@@ -1,4 +1,10 @@
+import { Hero } from '../components';
+
 function Landing() {
-  return <div className="text-4xl">Landing</div>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
 export default Landing;
